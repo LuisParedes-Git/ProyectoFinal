f6dc2330e99c1a314718a47cf5454c78ab3bf9bb
LuisParedes-Git <53629536+LuisParedes-Git@users.noreply.github.com>
2020-10-31 18:02:21 -0600

    Update server.php

--- server.php
+++ server.php
@@ -8,7 +8,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'luis', 'VFMv7n0GotPCIGxl', 'inventorymanagement');
+$db = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b4962aeb486a6f', '4f44c548', 'heroku_b2cde43a768c1fd');
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
