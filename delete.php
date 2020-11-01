0b188802be6d7aaeecdaae6c08a817c5d702a73d
LuisParedes-Git <53629536+LuisParedes-Git@users.noreply.github.com>
2020-10-31 18:33:03 -0600

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'inventorymanagement');
+$db = mysqli_connect('us-cdbr-east-02.cleardb.com', 'b4962aeb486a6f', '', 'heroku_b2cde43a768c1fd');
 if (mysqli_connect_errno())
     {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
